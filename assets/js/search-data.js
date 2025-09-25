f10f09b873eba3262e37aa19b2d0e8cb888a5229
jardin00 <104073898+jardin00@users.noreply.github.com>
2025-09-25 07:49:44 +0000

    Deploying to gh-pages from @ jardin00/myCV@c957d3b510e7174707cc51eadf15f84360f2c0f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,18 +412,18 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/myCV/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/myCV/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "news-i-graduated-from-the-department-of-industrial-and-systems-engineering-at-dongguk-university-cum-laude-gpa-4-1-4-5",
           title: 'I graduated from the Department of Industrial and Systems Engineering at Dongguk University!...',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/myCV/news/announcement_2/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
