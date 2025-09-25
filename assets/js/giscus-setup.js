7b97df05106fa56d3fb1bb45cae5f83278f0499b
jardin00 <104073898+jardin00@users.noreply.github.com>
2025-09-25 08:39:49 +0000

    Deploying to gh-pages from @ jardin00/jardin00.github.io@a4b2e64bd8d01c9a2906012ccce0fd2268a9d61b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jardin00/myCV",
+    "data-repo": "jardin00/jardin00.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
