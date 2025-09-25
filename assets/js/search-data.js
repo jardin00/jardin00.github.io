bdbcb7e311706c4e9e5226d75f12e2a67bbe8cfc
jardin00 <104073898+jardin00@users.noreply.github.com>
2025-09-25 08:11:28 +0000

    Deploying to gh-pages from @ jardin00/myCV@32aa8620515382fa1b6c36c4df918f839cdf1f1f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/myCV/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/myCV/people/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
