f15d6de7620bdcc3d0d4fa87453afa70e91abe7f
jardin00 <104073898+jardin00@users.noreply.github.com>
2025-09-25 07:45:55 +0000

    Deploying to gh-pages from @ jardin00/myCV@77a277cc5167765af177eafcfcd0c23753fd0b19 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,16 +412,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/myCV/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/myCV/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-graduated-from-the-department-of-industrial-and-systems-engineering-at-dongguk-university-cum-laude-gpa-4-1-4-5",
+          title: 'I graduated from the Department of Industrial and Systems Engineering at Dongguk University!...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
